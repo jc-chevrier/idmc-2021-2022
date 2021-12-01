884ef68f8daafd7fdddb0feac25c1197ed97ba44
jc-chevrier <47176536+jc-chevrier@users.noreply.github.com>
2021-12-01 11:24:00 +0100

    Ajout de la sécurité.

--- games-catalog-layer-based-architecture/src/main/java/fr/unilorraine/idmc/gamescatalog/controllers/GamesController.java
+++ games-catalog-layer-based-architecture/src/main/java/fr/unilorraine/idmc/gamescatalog/controllers/GamesController.java
@@ -17,8 +17,6 @@ import java.util.List;
 public class GamesController {
     private final GamesService gamesService;
 
-
-
     @GetMapping
     public List<GameView> findAll() {
         return gamesService.findAll();
--- games-catalog-layer-based-architecture/src/main/java/fr/unilorraine/idmc/gamescatalog/repositories/PublisherRepository.java
+++ games-catalog-layer-based-architecture/src/main/java/fr/unilorraine/idmc/gamescatalog/repositories/PublisherRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PublisherRepository extends CrudRepository<Publisher, Long> {
-}
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {}
